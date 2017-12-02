a6d0f417dc535ba154e3005533f8d7bc6b4c2438
DinossoLate <ignalelop@alum.us.es>
2017-12-02 17:40:06 +0100

    fix: Mejorado los mensajes de error de canVote
    Se han mejorado los mensajes de error de canVote para dar más información al usuario de la funcionalidad. Los cambios están subidos en la wiki del proyecto

--- src/processing.js
+++ src/processing.js
@@ -5,13 +5,12 @@ function canVote(user_id, election_id) {
     var election = auth.getElection(user_id, election_id);
     // Comprobamos si está vacia
     if(!election) {
-        // TODO: Cambiar a algo más descriptivo
-        return [false, "cant_vote"];
+        return [false, "election_not_found"];
     }
     // Ahora obtenemos el usuario
     var user = auth.getUser(user_id);
     if(!user) {
-        return [false, "cant_vote"];
+        return [false, "user_not_found"];
     }
     // Realizamos las comprobaciones de que pueda votar
     // Primero, comprobamos que la fecha de la votación no ha pasado:
@@ -27,7 +26,7 @@ function canVote(user_id, election_id) {
     // Si no está fuera del rango de votación.
     if(!(start_date < actual_date && end_date > actual_date)) {
         // TODO Cambiar a algo más descriptivo
-        return [false, "cant_vote"];
+        return [false, "election_not_open"];
     }
 
     // Despues hacemos comprobaciones del usuario
