19c33ff4284a889e73fc1433354d0cd2f8244f85
DinossoLate <ignalelop@alum.us.es>
2017-12-02 12:26:08 +0100

    fix: Mejorado la estructura del código. (Colaboradores: jorgoragu)
    Mejorada la posición de las comprobaciones para que el método de canVote tenga la lógica de negocio.
    
    IMPORTANTE
    Han surgido varios problemas en cuanto a las definiciones de los módulos de otros grupos, se encuentran en los comentarios del código de processing y authorization.

--- src/authorization.js
+++ src/authorization.js
@@ -1,40 +1,33 @@
 // le paso el username y me devuelve todos los datos del usuario si existe. 
 function getUser(user) {
 	// compruebo con .length ya que es más óptimo
-	if (user.length > 0) {
-		var user = 'jorgoragu';
-		var user_id = 30;
-		var user_group_id = 41;
-		var user_name = 'Jorge';
-		var user_surname = 'Gordo Aguilar';
-		var user_email = 'jorgoragu@alum.us.es';
-		var user_genre = Masculino;
-		var user_autonomous_community = 'Extremadura';
-		var user_age = 21;
-		var user_role = 'USUARIO';
-
-		return [user, user_id, user_group_id, user_name, user_surname, user_email, user_genre, user_autonomous_community, user_age, user_role];
-	
-	} else {
-		// En caso de que no se haya introducido un user devuelvo un mensaje de error
-		return "Debe introducir un nombre de usuario";
-	}
+	return {
+       username : "tansalalv",
+       name : "Tania",
+       surname : "Salguero Álvarez",
+       email : "mail@example.com",
+       genre : "Femenino",
+       autonomous_community : "Andalucía",
+       age : "21",
+       role : "USUARIO",
+	   // FIXME Esto no lo tiene la wiki de autorización!
+	   id_grupo: "31"
+   }
 }
 
 // Le paso el user_id y el election_id y compruebo restricciones. Si se cumplen devuelvo la encuesta
 function getElection(user_id, election_id) {
 	// Compruebo que existen ambos id	
-	if (user_id > 0 && election_id > 0) {
-		var user_group_id = 41;	// obtenemos el ID del grupo al que pertenece el usuario	
-		var election_group_id = 41; // obtenemos el ID del grupo que puede realizar la encuesta ¿y si la pueden realizar varios grupos?
-		var election_endDate = '31/12/2017 07:07'; // obtenemos la fecha de vencimiento de la votación
-		var date = new Date();
-		var rightNow = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
-			
-		if(user_group_id == election_group_id && election_endDate > rightNow) {	// comprobamos que el usuario pertenece al grupo que puede realizar la encuesta y que la fecha de finalización de la encuesta sea posterior a la fecha actual
-			return [election_id, election_censo_id, election_group_id, election_title, election_description, election_startDate, election_endDate];
-		}
-	}
-}
+	return {
+		id: "1",
+		id_censo: "288",
+		id_grupo: "31",
+		titulo: "Votación sobre consolas",
+		descripción: "En esta votación comprobaremos si hay mas gamers de PC o consolas",
+		fecha_ini: "31/07/2017 07:07",
+		fecha_fin: "31/08/2017 07:07"
+	};
+};
 		
-	
+exports.getUser = getUser;
+exports.getElection = getElection;
--- src/processing.js
+++ src/processing.js
@@ -1,4 +1,45 @@
+var auth = require('./authorization.js');
+
 function canVote(user_id, election_id) {
+    // Obtenemos la elección
+    var election = auth.getElection(user_id, election_id);
+    // Comprobamos si está vacia
+    if(!election) {
+        // TODO: Cambiar a algo más descriptivo
+        return [false, "cant_vote"];
+    }
+    // Ahora obtenemos el usuario
+    var user = auth.getUser(user_id);
+    if(!user) {
+        return [false, "cant_vote"];
+    }
+    // Realizamos las comprobaciones de que pueda votar
+    // Primero, comprobamos que la fecha de la votación no ha pasado:
+    // Al estar la fecha en un formato no reconocido por JS, necesitamos dividirlo y pasarlo por el constructor
+    var start_date = new Date(election.fecha_ini.substr(5, 4), election.fecha_ini.substr(2, 2)
+        ,election.fecha_ini.substr(0, 2),election.fecha_ini.substr(10, 2),
+         election.fecha_ini.substr(13, 2), 0);
+    var end_date = new Date(election.fecha_fin.substr(5, 4), election.fecha_fin.substr(2, 2)
+        ,election.fecha_fin.substr(0, 2),election.fecha_fin.substr(10, 2),
+         election.fecha_fin.substr(13, 2), 0);
+    var actual_date = new Date();
+
+    // Si no está fuera del rango de votación.
+    if(!(start_date < actual_date && end_date > actual_date)) {
+        // TODO Cambiar a algo más descriptivo
+        return [false, "cant_vote"];
+    }
+
+    // Despues hacemos comprobaciones del usuario
+    // Si el usuario no está en el grupo que puede votar:
+    if(user.id_grupo != election.id_grupo) {
+        // TODO Cambiar a algo más descriptivo
+        return [false, "cant_vote"];
+    }
+
+    // TODO Aqui iría la comprobación de doble voto, pero estamos a la espera de la wiki de almacenamiento.
+
+    // Si todo lo anterior es correcto, podemos admitir al usuario a votar
     return [true, "can_vote"];
 }
 
