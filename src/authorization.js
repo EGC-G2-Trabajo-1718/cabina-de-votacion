778f1989d09971574907400975117d8f6a81d422
yamikadesu <yamikadesu@outlook.es>
2017-12-06 18:11:42 +0100

    add:Añadidos métodos provisionales de obtención de preguntas y respuestas Se han añadido los métodos restantes aunque de forma provisional ya que falta confirmación del grupo de Administración de votaciones para ver si se hará de esa forma

--- src/authorization.js
+++ src/authorization.js
@@ -95,14 +95,44 @@ function getElection(election_id){
 	req.end();
 
 }
-//METODO PROVISIONAL, HAY QUE ESPERAR A QUE NOS DIGAN COMO HACERLO O SI VA DENTRO DE GETELECTION O SI HAY QUE HACER OTRO METODO MÁS PARA LAS RESPUESTAS
+
 function getQuestions(election_id){
 
 	var options = {
 		"method": "GET",
 		"hostname": url,
 		"port": 80,
-		"path":	"/api/get/preguntas.json?id="+election_id,
+		"path":	"/api/get/pregunta.json?id_votacion="+election_id,
+		"json":true
+	};
+
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf-8');
+		var responseString='';
+
+		res.on('data', function(data){
+			responseString += data;
+		});
+		res.on('end', function(){
+			console.log(responseString); //Muestra la respuesta por consola
+			var responseObject = JSON.parse(responseString);
+			success(responseObject);
+		});
+	});
+
+	req.write(dataString);
+
+	req.end();
+
+}
+
+function getAnswers(question_id){
+
+	var options = {
+		"method": "GET",
+		"hostname": url,
+		"port": 80,
+		"path":	"/api/get/respuesta.json?id_pregunta="+question_id,
 		"json":true
 	};
 
