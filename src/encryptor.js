640efbf10e934bde58d5b1603d101832af0686a4
DinossoLate <ignalelop@alum.us.es>
2017-12-08 12:12:40 +0100

    fix: Arreglados errores y mejorado el formato de encrypter.
    Se han arreglado algunos fallos no detectados en las pruebas unitarias realizadas y se ha mejorado la emisión del voto cifrado, ahora emitirá la cadena de números correspondientes a la representación numérica de los caracteres debido a que algunos de los caracteres eran no imprimibles. También se ha eliminado una de las dependencias, ya que la función realizada ya lo producía un método de las librerías estandar de node.js.
    
    IMPORTANTE
    La función ahora sí se puede considerar como completada y con el correcto funcionamiento.

--- src/encryptor.js
+++ src/encryptor.js
@@ -1,27 +1,25 @@
-var base64encoder = require('js-base64').Base64;
-
 // Está función solicita el voto como diccionario compuesto por el id de cada pregunta y su respuesta
 // y la clave de la votación
 function encrypt_vote(vote, key) {
     // Primero, obtenemos el voto como string.
     var vote_s = JSON.stringify(vote);
     // Después convertimos a base64.
-    var vote_b = base64encoder.encode(vote_s);
+    var vote_b = Buffer.from(vote_s).toString('base64');
 
-    // Dividimos los bloques
+    // Creamos el bloque
     var blocks = [vote_b];
     var blocks_size = vote_b.length;
     // Si el tamaño del string es mayor que la clave
     if(blocks_size > key.length) {
-        var new_blocks = [];
         // Crearemos un nuevo conjunto de bloques que tendrán asegurado un tamaño igual o menor (sólo el último) a la clave
-        for(var i = 0; i < blocks_size; i += key.length) {
+        var new_blocks = [];
+        for(var i = 0; i < Math.ceil(blocks_size/key.length); i++) {
             new_blocks.push(blocks[0].substring(i*key.length, (i+1)*key.length));
         }
         blocks = new_blocks;
     }
-    // Añadiremos padding al último bloque del conjunto
-    if(blocks[blocks.length-1] < key.length) {
+    // Añadiremos padding al último bloque del conjunto, sólo si el último bloque es menor que el tamaño de la clave
+    if(blocks[blocks.length-1].length < key.length) {
         // El nuevo último bloque será el último bloque más el caracter de padding por el número de caracteres necesarios para que tenga la misma cantidad que la clave
         blocks[blocks.length-1] = blocks[blocks.length-1].concat(multiply_string("-", (key.length - blocks[blocks.length-1].length)));
     }
@@ -30,16 +28,20 @@ function encrypt_vote(vote, key) {
     // Iteramos por cada bloque y ciframos por XOR cada bloque, añadiendo el resultado a un string final
     blocks.forEach(function(block) {
         result = result.concat(xor_cipher(block, key));
-        console.log("Cifrado de "+block+" es "+result);
     });
 
-    return result;
+    // Para terminar, convertimos el string en cadena de números, ya que muchos de los caracteres no serán imprimibles y pueden surgir problemas con esto.
+    var res_converted = new String();
+    for(var i = 0; i < result.length; i++) {
+        res_converted += result.charCodeAt(i).toString(10);
+    }
+    return res_converted;
 }
 
-// Esta función multiplicará por el exponente el caracter introducido.
-function multiply_string(character, exponent) {
+// Esta función multiplicará por el multiplicador el caracter introducido.
+function multiply_string(character, multiplier) {
     var result = character;
-    for(var i = 1; i < exponent; i++) {
+    for(var i = 1; i < multiplier; i++) {
         result = result.concat(character);
     }
     return result;
@@ -49,6 +51,7 @@ function multiply_string(character, exponent) {
 function xor_cipher(text, key) {
     var result = new String();
     for(var i = 0; i < text.length; i++) {
+        // Itera por cada caracter y cifra el resultado, conviriéndolo en string.
         result = result.concat(String.fromCharCode(text.charCodeAt(i) ^ key.charCodeAt(i)));
     }
     return result;
