75267e1eea732094670ce2b2fcb458c6bad2e0b8
darkgray <jorgegaguilar@hotmail.es>
2017-12-10 12:37:58 +0100

    fix: función vote en processing.js
    Corregido fallo y se han añadido comentarios en la función

--- src/processing.js
+++ src/processing.js
@@ -47,18 +47,24 @@ function canVote(user_id, election_id) {
 
 function vote(id_user, id_election, answers) {
 	var canvote = canVote(id_user, id_election);
+	// Comprobamos que el usuario pueda votar en esa encuesta usando la función canVote
 	if(canvote[1]=="can_vote"){
+	// Procedemos a encriptar 
         var encrypted_answers = encryptor.encrypt_vote(answers,0);
         // Comprobamos que el envío a almacenamiento es correcto:
-        if(!authorization.saveVote(encrypted_vote, id_election, id_user)) {
+        if(!authorization.saveVote(encrypted_answers, id_election, id_user)) {
+	    // Si no es correcto se mostrará el mensaje de error en el envío
             return [500, JSON.stringify({"result": false, "reason":"error_sending"})];
         } else {
+	    // En caso de éxito se enviará el voto 
             return [200,JSON.stringify({"result":true,"vote":{"id_user":id_user,"id_election":id_election,"encrypted_answers":encrypted_answers}})];
-        }
+        } 
+	// Tanto si no se muestra la votación como el usuario, en ambos casos se lanzará un código de estado 404 y la razón del mismo
 	}else if(canvote[1]=="election_not_found"){
 		return [404,JSON.stringify({"result":false,"reason":canvote[1]})];
 	}else if(canvote[1]=="user_not_found"){
 		return [404,JSON.stringify({"result":false,"reason":canvote[1]})];
+	// Para el resto de casos lanzaremos un código de estado 403 indicando que la acción no está permitida
 	}else{
 		return [403,JSON.stringify({"result":false,"reason":canvote[1]})];
 	}
