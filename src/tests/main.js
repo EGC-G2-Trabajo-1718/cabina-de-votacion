eaefdc137279201c72843e997758972ea8b6e471
DinossoLate <ignalelop@alum.us.es>
2018-01-12 18:05:28 +0100

    fix: Corregido las llamadas a la API
    Se han corregido el proceso para probar la llamada a la API anidando la función callback de las solicitudes. Esto debería corregir la integración continua de Travis.

--- src/tests/main.js
+++ src/tests/main.js
@@ -57,24 +57,22 @@ console.log(processing.vote(1, 1, AnswerJSON));
 
 // Prueba del sistema
 client.get('/api/check/vote.json?user=1&election=1', (err, req, res, obj) => {
-    if(err){
+    if (err) {
         console.error(err.stack);
     } else {
         console.log("Checked vote with user 1 and election 1");
-        count++;
+        // Si la anterior respuesta funciona, vamos a la segunda
+        client.post('/api/create/vote.json', {
+            id_user: "1", id_election: "1",
+            answers: ["respuesta1", "respuesta2", "respuesta3"]
+        }, (err, req, res, obj) => {
+            if (err) {
+                console.error(err.stack);
+            } else {
+                console.log('Created the vote ' + JSON.stringify(obj, null, 2));
+            }
+            // Al terminar ambas, cerramos el servidor
+            main.server.close();
+        });
     }
 });
-
-//Tests de vote
-client.post('/api/create/vote.json', {
-	id_user: "1", id_election: "1",
-	answers: ["respuesta1", "respuesta2", "respuesta3"]
-}, (err,req,res,obj) => {
-	if(err) console.error(err.stack);
-	else console.log('Created the vote '+JSON.stringify(obj,null,2));
-	count = count +1;
-});
-
-if(count==4){
-	main.server.close();
-}
